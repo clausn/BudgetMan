ea154bb08a6d29e403738eb8f2b87f242d1db1de
Paask <Paask@MIKAEL>
2017-08-30 12:35:38 +0200

    sac

--- test/test/Database.cs
+++ test/test/Database.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Data;
 using System.Data.SqlClient;
-using System.ComponentModel.DataAnnotations;
-using System.Data.Entity;
 using System.Configuration;
 using test.Models;
 
