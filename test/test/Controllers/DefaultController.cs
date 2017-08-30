5ada9466788f4637e4b1786e3cda4d9854e32fb7
Paask <Paask@MIKAEL>
2017-08-30 10:40:14 +0200

    j

--- test/test/Controllers/DefaultController.cs
+++ test/test/Controllers/DefaultController.cs
@@ -10,24 +10,6 @@ namespace test.Controllers
 {
     public class DefaultController : ApiController
     {
-        Transactions[] products = new Transactions[]
-        {
-            
-        };
-
-        public IEnumerable<Transactions> GetAllProducts()
-        {
-            return products;
-        }
-
-        public IHttpActionResult GetProduct(int id)
-        {
-            var product = products.FirstOrDefault((p) => p.Id == id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            return Ok(product);
-        }
+       
     }
 }
