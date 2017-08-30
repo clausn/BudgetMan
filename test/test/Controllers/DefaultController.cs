68c8dc54b03c8d1ad1721d5084b49eb8235f3f69
Paask <Paask@MIKAEL>
2017-08-30 11:45:54 +0200

    d

--- test/test/Controllers/DefaultController.cs
+++ test/test/Controllers/DefaultController.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-using test.Models;
+using System.Web;
+using System.Web.Mvc;
 
 namespace test.Controllers
 {
-    public class DefaultController : ApiController
+    public class DefaultController : Controller
     {
-       
+        // GET: Default
+        public ActionResult Index()
+        {
+            return Index();
+        }
     }
 }
