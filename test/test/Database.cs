c204f2dd7c24c8c0e1ae5bfce0fe9e6908f55f49
Claus <Claus@LENOVO-PC>
2017-08-30 12:35:18 +0200

    dsa

--- test/test/Database.cs
+++ test/test/Database.cs
@@ -5,7 +5,6 @@ using System.Web;
 using System.Data;
 using System.Data.SqlClient;
 using System.ComponentModel.DataAnnotations;
-using System.Data.Entity;
 using System.Configuration;
 using test.Models;
 
