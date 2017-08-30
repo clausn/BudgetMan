c2f2f8ebcbe51facb2d87fb2caa2a82da1aa2fbf
Paask <Paask@MIKAEL>
2017-08-30 10:25:09 +0200

    sdfn

---
+++ test/test/Models/Info.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test.Models
+{
+    public class Info
+    {
+        public int TransID { get; set; }
+        public float TransValue { get; set; }
+        public DateTime TransDateTime { get; set; }
+        public string TransText { get; set; }
+        public int Trans_FK_CatID { get; set; }
+
+        public int CatID { get; set; }
+        public string CatName { get; set; }
+        public int Cat_FK_CatID { get; set; }
+    }
+}
